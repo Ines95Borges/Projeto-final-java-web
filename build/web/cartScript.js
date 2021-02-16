6c4f4092a8a526ed9644ac8345cb30cc7709bc4f
Inês Barata Feio Borges <inesbarataborges@gmail.com>
2021-02-16 15:10:16 +0000

    merge done

--- build/web/cartScript.js
+++ build/web/cartScript.js
@@ -1,6 +1,5 @@
 // Mostrar o botão adicionar ao carrinho
 const addButtonDiv = document.querySelectorAll(".add-to-cart");
-const addButton = document.querySelectorAll(".add-to-cart-button");
 
 addButtonDiv.forEach(div => {
     div.addEventListener("mouseover", () => {
@@ -83,10 +82,10 @@ var Cart = class Cart {
     
     updateCartNumberItems(){
         var amount = document.querySelectorAll(".amount");
-        let numberItems = 0
+        let numberItems = 0;
         amount.forEach(p => {
             numberItems += parseInt(p.innerHTML, 10);
-        })
+        });
         return numberItems;
     }
 };
@@ -129,15 +128,5 @@ addButton.forEach(button => button.addEventListener("click", (e) => {
 addButtonDiv.forEach(div => {
     div.addEventListener("mouseleave", () => {
         div.childNodes[3].style.display = "none";
-    })
-})
-
-// Mostrar o carrinho de compras
-const shoppingCartButton = document.querySelector("#shopping-icon");
-const cart = document.querySelector(".cart");
-
-shoppingCartButton.addEventListener("click", () => cart.style.display = "block");
-
-// Fechar o carrinho de compras
-const cartCloseButton = document.querySelector("#close-cart");
-cartCloseButton.addEventListener("click", () => cart.style.display = "none");
+    });
+});
